e166f4d3a993318720219bc3ade98839c4a116ad
kkvanonymous <ninja.kunal01@gmail.com>
2021-05-20 17:44:09 +0530

    Configure webpack for multiple env

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import './styles.css'
 
 export const App = () =>{
     return <>
-    <h1>React Webpack Starter Template</h1>
+    <h1>React Webpack Starter Template- {process.env.NODE_ENV}</h1>
     </>
 }
