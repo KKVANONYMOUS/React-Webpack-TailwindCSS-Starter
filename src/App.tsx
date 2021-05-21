91094e813c86d7bfda5457d39e930097bbde57fc
kkvanonymous <ninja.kunal01@gmail.com>
2021-05-21 17:50:35 +0530

    Test Husky-3

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Counter from './Counter'
 import './styles.css'
 
 export const App = () => {
-  const name="Kunal"
+  const name = 'kkvanonymous'
   return (
     <>
       <h1>Hey, React Webpack Starter Template- {process.env.NODE_ENV}</h1>
