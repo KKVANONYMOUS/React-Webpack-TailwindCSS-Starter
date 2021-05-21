41ee455fc7bf5ae99b192fa799ddacb65e3a8ac8
kkvanonymous <ninja.kunal01@gmail.com>
2021-05-21 10:58:23 +0530

    Configure React Refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Counter from './Counter'
 import './styles.css'
 
 export const App = () =>{
     return <>
-    <h1>React Webpack Starter Template- {process.env.NODE_ENV}</h1>
+    <h1>Hey, React Webpack Starter Template- {process.env.NODE_ENV}</h1>
+    <Counter />
     </>
 }
