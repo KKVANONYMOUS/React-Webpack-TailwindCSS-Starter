4a44b5fa78638c2b1a9ce1a1d1886cbd5d781615
kkvanonymous <ninja.kunal01@gmail.com>
2021-05-21 17:37:36 +0530

    feat:husky
    Signed-off-by: kkvanonymous <ninja.kunal01@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Counter from './Counter'
 import './styles.css'
 
 export const App = () => {
+  const num = 0
   return (
     <>
       <h1>Hey, React Webpack Starter Template- {process.env.NODE_ENV}</h1>
