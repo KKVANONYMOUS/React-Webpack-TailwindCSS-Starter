46e4731f4dfedfbedf06f7e390008e57109cdb31
kkvanonymous <ninja.kunal01@gmail.com>
2021-05-21 17:41:35 +0530

    Check husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import Counter from './Counter'
 import './styles.css'
 
 export const App = () => {
-  const num = 0
+  const name="Kunal"
   return (
     <>
       <h1>Hey, React Webpack Starter Template- {process.env.NODE_ENV}</h1>
+      <h2>By - {name}</h2>
       <Counter />
     </>
   )
