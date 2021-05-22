4087f6594c2b2bf6e9488d962046d09b6633fccf
kkvanonymous <ninja.kunal01@gmail.com>
2021-05-22 16:53:30 +0530

    Add Intro Component
    Signed-off-by: kkvanonymous <ninja.kunal01@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
+import Intro from './components/Intro'
 import './styles/main.css'
 function App() {
   return (
-    <div className="h-64">
-      <div className="p-4 m-4 bg-green-600">
-        <h1 className="text-2xl font-bold text-white">Tailwind CSS Demo</h1>
-      </div>
-      <div className="p-4 m-4 bg-green-300 h-full">
-        <h2 className="text-green-900">Have much fun using Tailwind CSS</h2>
-        <button className="bg-purple-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          My Tailwind Button
-        </button>
-      </div>
-    </div>
+    <>
+      <Intro />
+    </>
   )
 }
 export default App
